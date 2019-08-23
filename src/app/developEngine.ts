d623f8c1a2a8486dbf94240562cde59a5cf217f9
Reza Fitriaman <fitriaman@bgenius.com>
2019-08-23 17:32:54 +0200

    add task on gulp file

--- src/app/developEngine.ts
+++ src/app/developEngine.ts
@@ -6,24 +6,31 @@ append html template to html index
 ------------------------------
 */
 
-fs.readdir('./dist/templates/html/', 'utf8', (err:any, files:any) => {
+// read the banners-template NAMES files from dist
+fs.readdir('./dist/banners/html/', 'utf8', (err:Function , files:Array<String>) => {
+    //if error throw err
     if(err) throw err;
-    //loop through the templates files-names
+    //loop through the banners-templates files-names
     for (let i = 0; i < files.length; i++) {
-        //read the content of the tempates file-names
-        let templateString = fs.readFileSync('./dist/templates/html/' + files[i]).toString();
+        // read the CONTENT of the banners-templates file
+        let templateString = fs.readFileSync('./dist/banners/html/' + files[i], 'utf8').toString();
+        // create regex to replace the content of the index.html with the banners-template file ( <!-- banner-320x100 --> )
         let regex = new RegExp("<!-- banner-" + files[i].split('.html')[0] + " -->", "g");
-        //if i is zero then read index file from src-map otherwise read it from dist-map
+        //if i is zero then read index.html file from src-folder otherwise read it from dist-folder (so in the beginning u have a clean index.html file again)
         if(i > 0) {
+            //read the CONTENT of the index.html on DIST folder
             let indexSrc = fs.readFileSync('./dist/app/index.html','utf8').toString();
+            //then replace <!-- banner-320x100 --> with the CONTENT of the banners-template file
             let newIndexSrc = indexSrc.replace(regex, templateString);
-            //write index-file to dist map
+            //write index-file to dist-folder
             fs.writeFileSync('./dist/app/index.html', newIndexSrc, 'utf8');
             console.log(`${files[i]} has been saved`)
         }else {
+            //read the CONTENT of the index.html on SRC folder (CLEAN INDEX.HTML FILE)
             let indexSrc = fs.readFileSync('./src/app/index.html','utf8').toString();
+            //then replace <!-- banner-320x100 --> with the CONTENT of the banners-template file
             let newIndexSrc = indexSrc.replace(regex, templateString);
-            //write index-file to dist map
+            //write index-file to dist-FOLDER
             fs.writeFileSync('./dist/app/index.html', newIndexSrc, 'utf8');
             console.log(`${files[i]} has been saved`)
         }
